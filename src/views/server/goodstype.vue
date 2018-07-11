abb52e3918924e0694b89898a270a1d1a9e5e402
½ÒÐñ»Ô <858152627@qq.com>
2018-07-11 19:09:59 +0800

    今日修改

--- src/views/server/goodstype.vue
+++ src/views/server/goodstype.vue
@@ -3,10 +3,15 @@
     <div class="filter-container">
       <el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="el-icon-edit">{{$t('table.add')}}</el-button>
     </div>
-    <tree-table :data="data" :evalFunc="func" style="width: 50%;" :evalArgs="args" :expandAll="expandAll" border>
+    <tree-table :data="data" :evalFunc="func" style="width: 80%;" :evalArgs="args" :expandAll="expandAll" border>
       <el-table-column label="名称">
         <template slot-scope="scope">
-          <span>{{scope.row.name}}</span>
+           <span>{{scope.row.name}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="banner">
+        <template slot-scope="scope">
+          <img class="link-type" @click="handleUpload(scope.row)" :src="scope.row.banner" width="40" height="40"/>
         </template>
       </el-table-column>
       <el-table-column label="操作" >
@@ -31,6 +36,31 @@
         <el-button v-else type="primary" @click="updateData">{{$t('table.confirm')}}</el-button>
       </div>
     </el-dialog>
+
+    <el-dialog title="上传图片" :visible.sync="uploaddialogFormVisible">
+      <el-form :rules="rules" ref="dialogForm" label-position="left" label-width="80px" style='width: 400px; margin-left:50px;'>
+          <el-upload
+            class="upload"
+            ref="upload"
+            action=""
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :before-upload="beforeupload" 
+            :file-list="fileList"
+            :auto-upload="true"
+            list-type="picture">
+            <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+            <!-- <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button> -->
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+          <!-- <el-progress v-show="showProgress" :text-inside="true" :stroke-width="15" :percentage="percentage"></el-progress> -->
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="uploaddialogFormVisible = false">{{$t('table.cancel')}}</el-button>
+        <el-button type="primary" @click="mainEdit">{{$t('table.confirm')}}</el-button>
+        <!-- <el-button v-else type="primary" @click="updateData">{{$t('table.confirm')}}</el-button> -->
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -45,7 +75,8 @@ import {
   goodstypes,
   addgoodstype,
   editgoodstype,
-  delgoodstype
+  delgoodstype,
+  upload
 } from "@/api/server";
 
 export default {
@@ -64,7 +95,8 @@ export default {
       },
       edittemp: {
         id: "",
-        name: ""
+        name: "",
+        banner: ""
       },
       dialogFormVisible: false,
       dialogStatus: "",
@@ -74,13 +106,80 @@ export default {
       },
       rules: {
         name: [{ required: true, message: "请输入类型名称", trigger: "blur" }]
-      }
+      },
+      uploaddialogFormVisible: false,
+      fileList: [],
+      file: {}
     };
   },
   created() {
     this.getList();
   },
   methods: {
+    handleUpload(row) {
+      this.fileList = [];
+      // console.log("row", row);
+      this.edittemp.id = row.id;
+      this.edittemp.name = row.name;
+      this.file.name = "banner";
+      if (row.banner !== "" && row.banner !== null) {
+        this.file.url = row.main;
+        this.fileList.push(this.file);
+      }
+      this.uploaddialogFormVisible = true;
+    },
+    beforeupload(file) {
+      if (this.fileList.length === 1) {
+        this.$message.error("只能上传一张banner哦~");
+        return;
+      }
+      console.log("beforefile:", file);
+      let param = new FormData(); // 创建form对象
+      param.append("file", file, file.name); // file对象是 beforeUpload参数
+      let config = {
+        headers: { "Content-Type": "multipart/form-data" }
+      };
+      upload(param).then(res => {
+        if (res.code === 200 && res.data) {
+          console.log(res);
+          this.file = {};
+          this.file.name = res.data.name;
+          this.file.url = "http://assets.v-islands.com/" + res.data.name;
+          this.fileList.push(this.file);
+          console.log("this.fileList:", this.fileList);
+        }
+      });
+      return false;
+    },
+    mainEdit() {
+      if (this.fileList.length > 0) {
+        this.edittemp.banner = this.fileList[0].url;
+      } else {
+        this.edittemp.banner = "";
+      }
+      console.log("edittemp:", this.edittemp);
+      editgoodstype(this.edittemp).then(res => {
+        console.log("res:", res);
+        if (res.code == 200) {
+          this.getList();
+          this.uploaddialogFormVisible = false;
+          this.$notify({
+            title: "成功",
+            message: "更新成功",
+            type: "success",
+            duration: 2000
+          });
+        }
+      });
+    },
+
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+      this.fileList = fileList;
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
     getList() {
       this.listLoading = true;
       goodstypes().then(response => {
@@ -138,10 +237,11 @@ export default {
           console.log("edittemp", this.temp);
           this.edittemp.id = this.temp.id;
           this.edittemp.name = this.temp.name;
+          this.edittemp.banner = this.temp.banner;
           const tempData = Object.assign({}, this.edittemp);
           // console.log("tempData", tempData);
           editgoodstype(tempData).then(res => {
-            console.log("res:", res);
+            // console.log("res:", res);
             if (res.code == 200) {
               this.getList();
               this.dialogFormVisible = false;
